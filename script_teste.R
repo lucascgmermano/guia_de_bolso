042a26f3de8689135ff18c091cca21aadbb5e1d1
Lucas Coraça Germano <lucascgermano@gmail.com>
2022-05-19 00:40:47 -0300

    gitgnore

--- script_teste.R
+++ script_teste.R
@@ -6,44 +6,16 @@ servr::daemon_stop(1)  # Desativar
 dados <- rio::import('https://raw.githubusercontent.com/seade-R/dados-covid-sp/master/data/dados_covid_sp.csv')
 sapply(X = dados, class)
 
-install.packages('readODS')
-dados <- readODS::read_ods(path = 'dados/planilha_ods.ods',
-                  sheet = 1,
-                  col_names = T,
-                  na = 5000
-                  )
-
-class(dados$distancia)
-tinytex::reinstall_tinytex()
-
-install.packages('tinytex', dependencies = T)
-install.packages('xelatex')
-update.packages(ask = FALSE, checkBuilt = TRUE)
-tinytex::tlmgr_update()
-tinytex::reinstall_tinytex()
-options(tinytex.verbose = TRUE)
-install.packages("TinyTeX")
-# ```{r, include=FALSE}
-# options(tinytex.verbose = TRUE)
-# ```
-
-readxl::read_excel(path = "dados/planilha_xlsx.xlsx",
-                   sheet = 1,
-                   col_names = c('vel','dist'),
-                   col_types = c("numeric","numeric"),
-                   range = "A3:B19")
-
-dir.create('teste')
+dir.create('ilustracoes')
 dir.exists(paths = dir())
-dirname(path = 'teste/')
-remove(... = 'teste/')
+remove(... = 'teste.txt')
 
 
-file.create("new_text_file.txt")
+file.create("teste.txt")
 file.create("new_word_file.docx")
 file.create("new_csv_file.csv")
 
-file.remove("new_csv_file.csv")
+file.remove("teste.txt")
 unlink("teste/", recursive = TRUE)
 unlink('new_csv_file.csv')
 dir()
@@ -52,9 +24,6 @@ dir()
 altura <- round(runif(n = 100, min = 130, max = 220),0)
 peso <- round(runif(n = 100, min = 40, max = 280),0)
 df <- data.frame(peso, altura)
-foreign::write.dbf(dataframe = df, file = 'dados/planilha_dbf.dbf')
-
-dados <- foreign::read.dbf('dados/planilha_dbf.dbf')
 
 iconv(x = ,
       from = ,
@@ -73,3 +42,11 @@ iconvlist()
 
 Encoding(x = c(dados::pinguins$especie, dados::pinguins$ilha))
 example("Encoding"1)
+dados <- dados::pinguins
+
+stringi::stri_enc_detect(dados$ilha[10])
+base::Encoding(dados$ilha) <- "UTF-8"
+dados$ilha <- base::enc2utf8(dados$ilha)
+dados$ilha <- as.character(dados$ilha)
+
+
