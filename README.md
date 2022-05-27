c00938caada0076e5ad7ff8f527da949ea1a5f34
Lucas Coraça Germano <lucascgermano@gmail.com>
2022-05-26 23:05:57 -0300

    rio::import

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-Sejam bem-vindos!  
+Bem vindo!
+Este livro traz um conjunto de  anotações de códigos R e métodos para execução de atividades específicas experimentadas anteriormente. Se trata da organização de rascunhos anteriores com objetivo de permitir a consulta de forma prática e rápida.   
 
-O objetivo deste livro é disponibilizar para consulta anotações de códigos R de forma prática e rápida. As explicações não serão  aprofundadas, tampouco se pretende esgotar as possibilidades do conteúdo apresentado, de modo que esta documentação deve ser utilizada somente como um guia rápido, pois consiste em um conjunto de rascunhos assimilados no dia-a-dia da manipulação de dados e na apresentação de resultados.  
+O conteúdo pode ser baixado nos formatos `.pdf` ou `epub`, mas o conteúdo é dinâmico, com atualizações frequentes. 
+A estrutura está disponível no [GitHub](https://github.com/lucascgmermano/guia_de_bolso.git) e críticas, sugestões ou contribuições de código podem ser enviadas para [lucascgermano@gmail.com](lucascgermano@gmail.com). 
 
-O conteúdo poderá ser baixado nos formatos `.pdf` ou `epub`, mas a proposta é que o conteúdo seja dinâmico, com atualizações semanais. 
-A estrutura de construção está disponível no [GitHub](https://github.com/lucascgmermano/guia_de_bolso.git).  
-Críticas, sugestões ou contribuições de código e conteúdo podem ser enviadas para [lucascgermano@gmail.com](lucascgermano@gmail.com). 
-
-Ficarei muito feliz, qualquer que seja o motivo do contato. 
--- script_teste.R
+++ script_teste.R
@@ -27,40 +27,11 @@ altura <- round(runif(n = 100, min = 130, max = 220),0)
 peso <- round(runif(n = 100, min = 40, max = 280),0)
 df <- data.frame(peso, altura)
 
-
-stringi::stri_enc_detect(str = dados::pinguins$especie[1])
-
-iconv(x = cars, 
-      from = "UTF-8", to = "ISO-8859-1")
-
-iconvlist()
-
-Encoding(x = c(dados::pinguins$especie, dados::pinguins$ilha))
-example("Encoding"1)
-dados <- dados::pinguins
-
-stringi::stri_enc_detect(dados$ilha[10])
-base::Encoding(dados$ilha) <- "UTF-8"
-dados$ilha <- base::enc2utf8(dados$ilha)
-dados$ilha <- as.character(dados$ilha)
-
-install.packages('writexl')
-
-writexl::write_xlsx()
-data.table::fwrite(x = iris, file = 'dados/iris_fwrite.csv', sep = ';')
-data.table::fread('dados/iris_fwrite.csv', nrows = 4)
-read.csv2('dados/iris.csv', nrows = 4)
-dados <- readr::write_csv2(x = iris, file = 'dados/iris.csv')
-read.csv2(file = 'dados/iris.csv', nrows = 4)
-
-data.table::fwrite(x = dados::pixar_avalicao_publico,
-                   file = 'dados/cars.csv',
-                   sep = ';', dateTimeAs = "squash")
-
-dados <- data.table::fread(file = 'dados/dados.csv', nrows = 20)
-data.table::fwrite(x = dados, 
-                   file = 'dados/dados20max.csv',
-                   append = TRUE, 
-                   sep = ';',
-                   showProgress = TRUE)
-
+read.csv2(file = file.path('dados','dados.csv'))
+read.csv2(file = here::here('dados','dados.csv'))
+here::here('dados','dados.csv')
+base::file.path('dados','dados.csv')  
+file
+file.path('dados/')
+here::here()
+rio::import(file = 'dados/dados.csv', format = ",")
