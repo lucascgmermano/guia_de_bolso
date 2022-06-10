3382227e03153d01d239f9ec6bd5164d64eebda1
Lucas Coraça Germano <lucascgermano@gmail.com>
2022-06-09 22:27:46 -0300

    export_list

--- script_teste.R
+++ script_teste.R
@@ -14,64 +14,39 @@ unlink(x = c('iris2.csv', 'iris3.csv'), recursive = T)
 
 install.packages("googlesheets4")
 
-rio::export(x = dados::diamante, file = 'dados/diamantes.xlsx')
-
 library(googlesheets4)
 url <- "https://docs.google.com/spreadsheets/d/18PV-XBQIrzB6Jfdvz5vgDHZPBoqlDxPcGxufOvCk5a0/edit#gid=0"
 
-setwd('../teste')
-write.csv2(x = iris[40:80,], file = 'iris2.csv')
 
 
-with(data = iris, expr = {Species == "versicolor"}) %>% sum()
-head(iris)
+#################################
+library(dplyr)
+iris
+filtro.fun(x = 'iris.csv')
+setwd('arquivos2/')
 
-funcao <- function(x) {
-  read.csv2(x) %>% 
-  dplyr::filter(Species=="versicolor")
+filtro.fun <- function(x){
+  data.table::fread(file = x) %>% 
+    dplyr::select("Petal.Length", "Petal.Width", "Species") %>%
+    dplyr::filter(Petal.Length > .5) %>%
+    setNames(nm = c("compr","largura","especie"))
 }
 
-arquivos <- list.files()
-dados <- lapply(X = arq, FUN = funcao) 
-
-Reduce(x = dados, f = rbind.data.frame)
-
-rio::import_list(file = arquivos, rbind = TRUE, header=T)[-7]
-data.table::f
-switch()
-function()
-lapply()
-library(dplyr)
-getwd()
-setwd(dir = '')
-lista <- base::list.files()
-arquivos <- base::lapply(X = lista,
-                         FUN = read.csv2)
-base::Reduce(x = arquivos,
-             f = base::rbind.data.frame)
-
-ler_arq <- function(x){
-  utils::read.csv2(file = x) %>% 
-    dplyr::filter(Species == 'versicolor')
-  
-  
-  lista <- base::list.files(path = 'arquivos/')     # Captura os arquivos na pasta e atribui à uma lista
-  arquivos <- base::lapply(X = lista,   # Lista com os arquivos a serem lidos
-                           FUN = read.csv2)  # Função escolhida para ler
-  base::Reduce(x = arquivos,       # Lista de dataframes
-                         f = base::merge)  # Função para empilhar
-a <- iris
-b <- iris 
-
-merge(x = a, y = b)
-
-data.table::rbindlist()
-
-setwd('arquivos/')
-
 lista <- list.files()
+arquivos <- lapply(X = lista, FUN = filtro.fun)
 
-tempfile(fileext = ".csv")
-export()
 
-rio::import_list(file = dir(path = 'arquivos/'), 1)
+data.table::rbindlist(l = arquivos)
+base::Reduce(x = arquivos, f = rbind.data.frame)
+
+setwd("~/Documentos/Estudos R/guia_de_bolso") # Volta para diretório inicial
+getwd()
+
+
+rio::export_list(x = list(iris1 = iris[1:30,], iris2 = iris[60:80,]),
+                 file = c("iris1.csv","iris2.csv"), sep=";")
+
+rio::export_list(x = list(iris1 = iris[1:30,], 
+                          iris2 = iris[60:80,],
+                          iris3 = iris[50:70,]),
+                 file = '%s.csv', sep=";")
